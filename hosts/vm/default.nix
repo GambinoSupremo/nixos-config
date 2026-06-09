b25b8008371535afcb7d789d56b5fe27c32957e6
Gavin <service.haiku882@passinbox.com>
2026-06-09 06:16:25 -0700

    Update default.nix

--- hosts/vm/default.nix
+++ hosts/vm/default.nix
@@ -14,25 +14,6 @@
 
   networking.hostName = "nix-vm";
 
-  # Use SDDM for the VM so we can choose sessions interactively.
-  services.greetd.enable = lib.mkForce false;
-
-  services.displayManager.sddm = {
-    enable = true;
-    wayland.enable = true;
-  };
-
-  # Preselect Mango if SDDM sees the session name.
-  # If this exact name does not match, SDDM will still show the session picker.
-  services.displayManager.defaultSession = "mango";
-
-  # Make sure sessions are visible to the display manager.
-  services.displayManager.sessionPackages = [
-    config.programs.mango.package
-    pkgs.hyprland
-    pkgs.niri
-  ];
-
   services.qemuGuest.enable = true;
   services.spice-vdagentd.enable = true;
   hardware.graphics.enable = true;
