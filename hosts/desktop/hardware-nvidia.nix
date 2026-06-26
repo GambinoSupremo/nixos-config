684c64cb8a31f83c8901958fd857731399605ca6
Gavin <service.haiku882@passinbox.com>
2026-06-26 13:58:10 -0700

    Update hardware-nvidia.nix

--- hosts/desktop/hardware-nvidia.nix
+++ hosts/desktop/hardware-nvidia.nix
@@ -1,20 +1,14 @@
 { config, pkgs, lib, ... }:
-
 {
   services.xserver.videoDrivers = [ "nvidia" ];
-
   hardware.nvidia.modesetting.enable  = true;
   hardware.nvidia.open                = false;
   hardware.nvidia.nvidiaSettings      = true;
   hardware.nvidia.package             = config.boot.kernelPackages.nvidiaPackages.stable;
-
   hardware.graphics.enable       = true;
   hardware.graphics.enable32Bit  = true;   # 32-bit libs for Steam/Proton
 
   environment.sessionVariables = {
-    NIXOS_OZONE_WL            = "1";
-    GBM_BACKEND               = "nvidia-drm";
-    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-    WLR_NO_HARDWARE_CURSORS   = "1";
+    NIXOS_OZONE_WL = "1";
   };
 }
